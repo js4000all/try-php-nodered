46fb39c3aa86e3d6a4515e6c2c22277ec4d5faf1
js4000all <js4000all@gmail.com>
2025-05-24 01:24:12 +0900

    docker-compose.ymlにinfluxdbサービスを追加。phpコンテナにinfluxdbクライアントをインストール

--- Dockerfile.php
+++ Dockerfile.php
@@ -14,6 +14,13 @@ RUN set -x \
     && rm -rf /var/lib/apt/lists/* \
     && useradd -m -s /bin/bash app
 
+WORKDIR /tmp
+RUN set -x \
+    && curl -L https://dl.influxdata.com/influxdb/releases/influxdb2-client-2.7.5-linux-amd64.tar.gz -O \
+    && tar xvzf ./influxdb2-client-2.7.5-linux-amd64.tar.gz \
+    && mv ./influx /usr/local/bin/ \
+    && rm -rf influxdb2-client-2.7.5-linux-amd64.tar.gz
+
 COPY ./profile.d /etc/profile.d
 
 WORKDIR /var/www/html
